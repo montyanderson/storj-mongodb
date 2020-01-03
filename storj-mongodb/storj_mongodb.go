de92a4282799b776af2f450e4cfb7bd3614f6cc0
Karl Mozurkewich <karl@utropicmedia.com>
2020-01-02 23:40:01 -0500

    updated to libuplink 0.27.1 with macaroon support.

--- storj-mongodb/storj_mongodb.go
+++ storj-mongodb/storj_mongodb.go
@@ -14,7 +14,7 @@ import (
 	"unsafe"
 
 	"utropicmedia/mongodb_storj_interface/mongo"
- 	"utropicmedia/mongodb_storj_interface/storj"
+	"utropicmedia/mongodb_storj_interface/storj"
 
 	"github.com/urfave/cli"
 )
@@ -31,8 +31,8 @@ var app = cli.NewApp()
 func setAppInfo() {
 	app.Name = "Storj MongoDB Connector"
 	app.Usage = "Backup your MongoDB collections to the decentralized Storj network"
-	app.Author = "Satyam Shivam - Utropicmedia"
-	app.Version = "1.0.9"
+	app.Authors = []*cli.Author{{Name: "Satyam Shivam - Utropicmedia", Email: "development@utropicmedia.com"}}
+	app.Version = "1.0.10"
 
 }
 
@@ -46,24 +46,24 @@ func setDebug(debugVal bool) {
 // setCommands sets various command-line options for the app.
 func setCommands() {
 
-	app.Commands = []cli.Command{
+	app.Commands = []*cli.Command{
 		{
 			Name:    "parse",
 			Aliases: []string{"p"},
 			Usage:   "Command to read and parse JSON information about MongoDB instance properties and then fetch ALL its collections. ",
 			//\narguments-\n\t  fileName [optional] = provide full file name (with complete path), storing mongoDB properties if this fileName is not given, then data is read from ./config/db_connector.json\n\t  example = ./storj_mongodb d ./config/db_property.json\n",
-			Action: func(cliContext *cli.Context) {
+			Action: func(cliContext *cli.Context) error {
 				var fullFileName = dbConfigFile
 
 				// process arguments
-				if len(cliContext.Args()) > 0 {
-					for i := 0; i < len(cliContext.Args()); i++ {
+				if len(cliContext.Args().Slice()) > 0 {
+					for i := 0; i < len(cliContext.Args().Slice()); i++ {
 
 						// Incase, debug is provided as argument.
-						if cliContext.Args()[i] == "debug" {
+						if cliContext.Args().Slice()[i] == "debug" {
 							setDebug(true)
 						} else {
-							fullFileName = cliContext.Args()[i]
+							fullFileName = cliContext.Args().Slice()[i]
 						}
 					}
 				}
@@ -72,7 +72,8 @@ func setCommands() {
 				dbReader, err := mongo.ConnectToDB(fullFileName)
 				//
 				if err != nil {
-					log.Fatalf("Failed to establish connection with MongoDB: %s\n", err)
+					fmt.Printf("Failed to establish connection with MongoDB:")
+					return err
 				}
 
 				// Connect to the Database and process data
@@ -87,6 +88,7 @@ func setCommands() {
 				if gbDEBUG {
 					fmt.Println("Size of fetched data from database :", dbReader.DatabaseName, unsafe.Sizeof(data))
 				}
+				return err
 			},
 		},
 		{
@@ -94,20 +96,34 @@ func setCommands() {
 			Aliases: []string{"t"},
 			Usage:   "Command to read and parse JSON information about Storj network and upload sample JSON data",
 			//\n arguments- 1. fileName [optional] = provide full file name (with complete path), storing Storj configuration information if this fileName is not given, then data is read from ./config/storj_config.json example = ./storj_mongodb s ./config/storj_config.json\n\n\n",
-			Action: func(cliContext *cli.Context) {
+			Action: func(cliContext *cli.Context) error {
 
 				// Default Storj configuration file name.
 				var fullFileName = storjConfigFile
+				var foundFirstFileName = false
+				var foundSecondFileName = false
+				var keyValue string
+				var restrict string
 
 				// process arguments
-				if len(cliContext.Args()) > 0 {
-					for i := 0; i < len(cliContext.Args()); i++ {
+				if len(cliContext.Args().Slice()) > 0 {
+					for i := 0; i < len(cliContext.Args().Slice()); i++ {
 
 						// Incase, debug is provided as argument.
-						if cliContext.Args()[i] == "debug" {
+						if cliContext.Args().Slice()[i] == "debug" {
 							setDebug(true)
 						} else {
-							fullFileName = cliContext.Args()[i]
+							if !foundFirstFileName {
+								fullFileName = cliContext.Args().Slice()[i]
+								foundFirstFileName = true
+							} else {
+								if !foundSecondFileName {
+									keyValue = cliContext.Args().Slice()[i]
+									foundSecondFileName = true
+								} else {
+									restrict = cliContext.Args().Slice()[i]
+								}
+							}
 						}
 					}
 				}
@@ -131,13 +147,14 @@ func setCommands() {
 				}
 
 				// Create a buffer as an io.Reader implementor.
-				buf := bytes.NewBuffer(bsonData)
+				buf1 := bytes.NewBuffer(bsonData)
 				//
-				err := storj.ConnectStorjReadUploadData(fullFileName, buf, dbName)
+				_, err := storj.ConnectStorjReadUploadData(fullFileName, buf1, dbName, keyValue, restrict)
 				//
 				if err != nil {
 					fmt.Println("Error while uploading data to the Storj bucket")
 				}
+				return err
 			},
 		},
 		{
@@ -145,25 +162,39 @@ func setCommands() {
 			Aliases: []string{"s"},
 			Usage:   "Command to connect and transfer ALL collections from a desired MongoDB instance to given Storj Bucket in BSON format",
 			//\n    arguments-\n      1. fileName [optional] = provide full file name (with complete path), storing mongoDB properties in JSON format\n   if this fileName is not given, then data is read from ./config/db_property.json\n      2. fileName [optional] = provide full file name (with complete path), storing Storj configuration in JSON format\n     if this fileName is not given, then data is read from ./config/storj_config.json\n   example = ./storj_mongodb c ./config/db_property.json ./config/storj_config.json\n",
-			Action: func(cliContext *cli.Context) {
+			Action: func(cliContext *cli.Context) error {
 
 				// Default configuration file names.
 				var fullFileNameStorj = storjConfigFile
 				var fullFileNameMongoDB = dbConfigFile
+				var keyValue string
+				var restrict string
 
 				// process arguments - Reading fileName from the command line.
 				var foundFirstFileName = false
-				if len(cliContext.Args()) > 0 {
-					for i := 0; i < len(cliContext.Args()); i++ {
+				var foundSecondFileName = false
+				var foundThirdFileName = false
+				if len(cliContext.Args().Slice()) > 0 {
+					for i := 0; i < len(cliContext.Args().Slice()); i++ {
 						// Incase debug is provided as argument.
-						if cliContext.Args()[i] == "debug" {
+						if cliContext.Args().Slice()[i] == "debug" {
 							setDebug(true)
 						} else {
 							if !foundFirstFileName {
-								fullFileNameMongoDB = cliContext.Args()[i]
+								fullFileNameMongoDB = cliContext.Args().Slice()[i]
 								foundFirstFileName = true
 							} else {
-								fullFileNameStorj = cliContext.Args()[i]
+								if !foundSecondFileName {
+									fullFileNameStorj = cliContext.Args().Slice()[i]
+									foundSecondFileName = true
+								} else {
+									if !foundThirdFileName {
+										keyValue = cliContext.Args().Slice()[i]
+										foundThirdFileName = true
+									} else {
+										restrict = cliContext.Args().Slice()[i]
+									}
+								}
 							}
 						}
 					}
@@ -171,18 +202,29 @@ func setCommands() {
 
 				// Establish connection with MongoDB and get io.Reader implementor.
 				dbReader, err := mongo.ConnectToDB(fullFileNameMongoDB)
-				//
-				if err != nil {
-					log.Fatalf("Failed to establish connection with MongoDB: %s\n", err)
-				}
 
+				if err != nil {
+					fmt.Printf("Failed to establish connection with MongoDB:\n")
+					return err
+				}
 				// Fetch all collections' documents from MongoDB instance
 				// and simultaneously store them into desired Storj bucket.
-				err = storj.ConnectStorjReadUploadData(fullFileNameStorj, dbReader, dbReader.DatabaseName)
-				//
+				scope, err := storj.ConnectStorjReadUploadData(fullFileNameStorj, dbReader, dbReader.DatabaseName, keyValue, restrict)
 				if err != nil {
-					log.Fatalf("Error while fetching MongoDB documents and uploading them to bucket: %s\n", err)
+					fmt.Printf("Error while fetching MongoDB documents and uploading them to bucket:")
+					return err
 				}
+				fmt.Println(" ")
+				if keyValue == "key" {
+					if restrict == "restrict" {
+						fmt.Println("Restricted Serialized Scope Key: ", scope)
+						fmt.Println(" ")
+					} else {
+						fmt.Println("Serialized Scope Key: ", scope)
+						fmt.Println(" ")
+					}
+				}
+				return err
 			},
 		},
 	}
